89deed391d6482ddfd17f9e989a6be8749b4923b
MatiasForconi <64165844+MatiasForconi@users.noreply.github.com>
2020-05-28 00:06:01 -0300

    Parcial terminado

---
+++ README.md
@@ -0,0 +1,6 @@
+# Parcial nº 1 - LPPA 
+## Formulario de registro
+Este formulario contiene varios campos para que el usuario ingrese sus datos.
+
+
+Para ver este formulario online visitar este [link](https://github.com/MatiasForconi/Parcial-1-LPPA).
---
+++ js/app.js
@@ -0,0 +1,57 @@
+function validar() {
+  var username, userlastname, useremail, age, gender, interest, country,
+  comentary;
+  username = document.getElementById("username").value;
+  userlastname = document.getElementById("userlastname").value;
+  useremail = document.getElementById("useremail").value;
+  age = document.getElementById("age").value;
+  gender = document.getElementById("gender").checked;
+  interest = document.getElementById("interest").checked;
+  country = document.getElementById("country").value;
+  comentary = document.getElementById("comentary").value;
+  console.log(username);
+  console.log(userlastname);
+  console.log(useremail);
+  console.log(age);
+  console.log(gender);
+  console.log(interest);
+  console.log(country);
+  console.log(comentary);
+
+if (username === "" ||userlastname === "" || useremail === "" || age === "" ||
+gender === "" || interest === "" || country === "" || comentary === "") {
+  alert("Los campos son obligatorios");
+  return false;
+}
+
+else if(username.length < 3) {
+  alert("El nombre debe tener mas de 3 caracateres");
+  return false;
+}
+
+else if(userlastname.length < 3) {
+  alert("El apellido debe tener mas de 3 caracateres");
+  return false;
+}
+
+else if(useremail.length > 20) {
+  alert("El correo es muy largo");
+  return false;
+}
+
+else if(age > 100) {
+  alert("La edad ingresada no esta permitida");
+  return false; 
+}
+
+else if (gender.checked){
+  alert("Debe seleccionar una opcion sobre el genero");
+  return false;
+}
+
+else if(interest.checked){
+  alert("Debe seleccionar un tema de interes");
+  return false;
+}
+
+}
